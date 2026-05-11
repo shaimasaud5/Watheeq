291b5d3a255772b2c172615ccc234d5b5063c07a
shaimasaud <148467435+shaimasaud@users.noreply.github.com>
2026-05-11 19:19:05 +0300

    fix: namespaced success_url

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -22,13 +22,15 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='frontend/auth/password_reset.html',
         email_template_name='frontend/auth/password_reset_email.txt',
+        success_url=reverse_lazy('frontend:password_reset_done'),
     ), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='frontend/auth/password_reset_done.html'),
         name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='frontend/auth/password_reset_confirm.html'),
-        name='password_reset_confirm'),
+        template_name='frontend/auth/password_reset_confirm.html',
+        success_url=reverse_lazy('frontend:password_reset_complete'),
+    ), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='frontend/auth/password_reset_complete.html'),
         name='password_reset_complete'),
