7a61e71c41932beefc346e02f517a8f6dff7acad
shaimasaud <148467435+shaimasaud@users.noreply.github.com>
2026-05-11 18:41:29 +0300

    fix: merge urlpatterns

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -155,8 +155,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 65
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # App Password مو الباسورد العادي
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+
